90d46e9d8a1a57bc3b4151a9873302744c8a40aa
Adnane Hamdy <a.hamdi@devaxy.com>
2024-07-18 15:55:20 +0100

    Ajoutez des fichiers projet.

---
+++ RamenerDesAdhésions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+class Program
+{
+    public static void Main()
+    { 
+        string connectionString = "Data Source=HAMDY-ADNANE\\SQLEXPRESS;Initial Catalog=DatabaseAyoub;Integrated Security=True";
+        string query = @"
+             SELECT * FROM DIM_ADHESION_15072024
+             WHERE NOM IS NOT NULL AND PRENOM IS NOT NULL AND DATE_NAISSANCE IS NOT NULL;"
+        ;
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataReader lecteur = command.ExecuteReader();
+            while (lecteur.Read())
+            {
+                Console.WriteLine($"Nom: {lecteur["NOM"]}, Prénom: {lecteur["PRENOM"]}, Date de Naissance: {lecteur["DATE_NAISSANCE"]}");
+            }
+            lecteur.Close();
+        }
+    }
+}
