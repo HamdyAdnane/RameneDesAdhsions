ea8717d7d7ee47c69e860d709e40442316f1b8f3
Adnane Hamdy <a.hamdi@devaxy.com>
2024-08-02 15:00:43 +0100

    Ajouter Configuration String

---
+++ RamenerDesAdhésions/DIM_ADHESION_15072024.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RamenerDesAdhésions
+{
+    public class DIM_ADHESION_15072024
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string DateNaissance { get; set; }
+        public override string ToString()
+        {
+            return $"Nom :{Nom}    Prenom:{Prenom}    Date Naissance :{DateNaissance}";
+        }
+    }
+}
--- RamenerDesAdhésions/Program.cs
+++ RamenerDesAdhésions/Program.cs
@@ -1,23 +1,34 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Data;
 using System.Data.SqlClient;
+using RamenerDesAdhésions;
+
 class Program
 {
     public static void Main()
-    { 
-        string connectionString = "Data Source=HAMDY-ADNANE\\SQLEXPRESS;Initial Catalog=DatabaseAyoub;Integrated Security=True";
+    {
+        var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
         string query = @"
              SELECT * FROM DIM_ADHESION_15072024
              WHERE NOM IS NOT NULL AND PRENOM IS NOT NULL AND DATE_NAISSANCE IS NOT NULL;"
         ;
-        using (SqlConnection connection = new SqlConnection(connectionString))
+        using (SqlConnection connection = new SqlConnection(configuration.GetSection("ConnectionString").Value))
         {
             connection.Open();
-            SqlCommand command = new SqlCommand(query, connection);
-            SqlDataReader lecteur = command.ExecuteReader();
+            SqlCommand commande = new SqlCommand(query, connection);
+            SqlDataReader lecteur = commande.ExecuteReader();
+            DIM_ADHESION_15072024 Dim_Adhesion;
             while (lecteur.Read())
             {
-                Console.WriteLine($"Nom: {lecteur["NOM"]}, Prénom: {lecteur["PRENOM"]}, Date de Naissance: {lecteur["DATE_NAISSANCE"]}");
+                Dim_Adhesion = new DIM_ADHESION_15072024
+                {
+                    Nom = lecteur.GetString("NOM"),
+                    Prenom = lecteur.GetString("PRENOM"),
+                    DateNaissance = lecteur.GetString("DATE_NAISSANCE")
+                };
+                Console.WriteLine(Dim_Adhesion);
             }
             lecteur.Close();
         }
